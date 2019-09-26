20b0b5fa744024453e207821c474ea35c8789928
Björn Andersson <bjoand12@gmail.com>
2019-09-26 19:40:06 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel=icon href=bild8.png sizes="16x16" type="image/png">
+    <link rel="stylesheet" type="text/css" href="style/header.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="style/first.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="style/second.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="style/third.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="style/fourth.css" media="screen" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <title>Nordic Web</title>
+    <meta name="description" content="We as Nordic Web will provide you the online presence you deserve.">
+</head>
+<body>
+    hi
+</body>
+</html>
